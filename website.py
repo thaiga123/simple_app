76f835e61ccfe82ebb8e6837ff92f2c6d27c3c2b
thaiga123 <ajmeyer915@gmail.com>
2024-11-28 15:57:13 +0200

    Initial commit for New_app

---
+++ website.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/home")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/continue")
+def continue_():
+    return render_template("next_page.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
